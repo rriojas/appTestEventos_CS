d300033b085dc303ae728b336a0e9e9cd108f592
Ruben Miguel Riojas Rodriguez <rriojas@hotmail.com>
2021-11-22 13:20:35 -0600

    Add project files.

---
+++ appTestEventos_CS/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace appTestEventos_CS
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.picFrame = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.picFrame)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // picFrame
+      // 
+      this.picFrame.Image = ((System.Drawing.Image)(resources.GetObject("picFrame.Image")));
+      this.picFrame.Location = new System.Drawing.Point(444, 251);
+      this.picFrame.Name = "picFrame";
+      this.picFrame.Size = new System.Drawing.Size(50, 50);
+      this.picFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.picFrame.TabIndex = 0;
+      this.picFrame.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(991, 599);
+      this.Controls.Add(this.picFrame);
+      this.KeyPreview = true;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+      ((System.ComponentModel.ISupportInitialize)(this.picFrame)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox picFrame;
+  }
+}
+
---
+++ appTestEventos_CS/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appTestEventos_CS
+{
+  public partial class Form1 : Form
+  {
+    private Image[] pacmanImage = new Image[4];
+    private int currentMouthPosition = 0;
+    private int xPosition = 0;
+    private int yPosition = 0;
+
+    // The index of the current frame.
+    private int FrameNum = 0;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+      switch (e.KeyCode)
+      {
+        case Keys.Left:
+        currentMouthPosition = 0;
+        picFrame.Image = Image.FromFile(@"pac-izquierda.png");
+        picFrame.Location = new Point(picFrame.Location.X - 10, picFrame.Location.Y);
+        if (picFrame.Location.X < 0)
+        {
+          picFrame.Location = new Point(this.Width, picFrame.Location.Y);
+        }
+        break;
+        case Keys.Right:
+        currentMouthPosition = 1;
+        picFrame.Image = Image.FromFile(@"pac-derecha.png");
+        picFrame.Location = new Point(picFrame.Location.X + 10, picFrame.Location.Y);
+        if (picFrame.Location.X >= this.Width )
+        {
+          picFrame.Location = new Point(0, picFrame.Location.Y);
+        }
+        break;
+        case Keys.Up:
+        currentMouthPosition = 2;
+        picFrame.Image = Image.FromFile(@"pac-arriba.png");
+        picFrame.Location = new Point(picFrame.Location.X, picFrame.Location.Y-10);
+        break;
+        case Keys.Down:
+        currentMouthPosition = 3;
+        picFrame.Image = Image.FromFile(@"pac-abajo.png");
+        picFrame.Location = new Point(picFrame.Location.X, picFrame.Location.Y+10);
+        break;
+      }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
